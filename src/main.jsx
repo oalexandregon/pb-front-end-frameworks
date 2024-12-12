9d489712da40441bb1971ea71ea01b86b752bb1e
Alexandre Goncalves <avg1202@gmail.com>
2024-12-12 19:24:20 -0300

    Correção em rotas protected

--- src/main.jsx
+++ src/main.jsx
@@ -2,35 +2,56 @@ import { createRoot } from 'react-dom/client'
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AppProvider } from "./context";
-import Home from "./pages/home/home"
-import Details from "./pages/details/details"
+import Home from "./pages/home/home";
+import Details from "./pages/details/details";
 import "./global.css";
-import Favorites from "./pages/favorites/favorites"
+import Favorites from "./pages/favorites/favorites";
 import TvSeries from './pages/tvShows/tvShows';
-import Movies from "./pages/movies/movies"
+import Movies from "./pages/movies/movies";
 import SignUp from './pages/signUp/signUp';
 import SignIn from './pages/signIn/signIn';
+import ProtectedRoute from './routes/protected';  
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: (
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/:id",
-    element: <Details />,
+    element: (
+      <ProtectedRoute>
+        <Details />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/favoritos/",
-    element: <Favorites />,
+    element: (
+      <ProtectedRoute>
+        <Favorites />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/filmes/",
-    element: <Movies />,
+    element: (
+      <ProtectedRoute>
+        <Movies />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/tv/",
-    element: <TvSeries />,
+    element: (
+      <ProtectedRoute>
+        <TvSeries />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/signUp/",
@@ -42,15 +63,10 @@ const router = createBrowserRouter([
   },
 ]);
 
-
-
-
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProvider>
       <RouterProvider router={router} />
     </AppProvider>
   </React.StrictMode>
-)
-
+);
